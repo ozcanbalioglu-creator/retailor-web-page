581b02854873ea1d2acb01d21b32d64f06462df4
Özcan <ozcanbalioglu@gmail.com>
2026-02-11 15:17:45 +0300

    Fix: Configure for custom domain retailor.com.tr with root base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: '/retailor-web-page/',
+    base: '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
